9568eb5f39f01fc737008735568f538eba81cc4f
Aubrey Polk <aubstacle3@gmail.com>
2020-12-21 14:06:29 -0500

    navbar added, necessary libraries installed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
-import Image from "react-bootstrap/Image";
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
-      <Link to="/" className="navbar-brand knight-font">
-        <Image className="py-auto pr-1" src="images/knightIcon.png" />
-        Game Knight
+    <nav className="navbar navbar-dark">
+      <Link to="/" className="navbar-brand">
+        Aubrey Polk
       </Link>
       <button
         className="navbar-toggler"
@@ -21,33 +19,20 @@ const Navbar = (props) => {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav ml-auto knight-font">
-          <NavLink to="/events" className="nav-link">
-            Events
+      <div className="collapse navbar-collapse text-right" id="navbarNavAltMarkup">
+        <div className="navbar-nav ml-auto">
+          <NavLink to="/" className="nav-link">
+            Home
           </NavLink>
-          <NavLink to="/dashboard" className="nav-link">
-            Dashboard
+          <NavLink to="/portfolio" className="nav-link">
+            Portfolio
           </NavLink>
-          
           <NavLink to="/about" className="nav-link">
             About
           </NavLink>
-          {!props.jwt ? (
-            <NavLink to="/login" className="nav-link">
-              Login
-            </NavLink>
-          ) : (
-            <NavLink
-              to="/"
-              onClick={() => {
-                props.setJwt("");
-              }}
-              className="nav-link"
-            >
-              Logout
-            </NavLink>
-          )}
+          <NavLink to="/contact" className="nav-link">
+            Contact
+          </NavLink>
         </div>
       </div>
     </nav>
