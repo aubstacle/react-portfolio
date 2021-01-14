a27b9fe9bf5a487404acdecdc228d932a1f0087b
Aubrey Polk <aubstacle3@gmail.com>
2021-01-14 00:44:21 -0500

    portfolio page jumbotron

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import Container from "../../components/Container/Container";
-import "../Home/Home.css";
+import "../Portfolio/Portfolio.css";
 import Jumbotron from "react-bootstrap/Jumbotron";
 
 const Portfolio = () => {
@@ -8,15 +8,11 @@ const Portfolio = () => {
       className="flex-fill home-bg"
       containerType="container-fluid mb-5"
     >
-      <Jumbotron fluid>
-        <Container>
-          <h1>Fluid jumbotron</h1>
-          <p>
-            This is a modified jumbotron that occupies the entire horizontal
-            space of its parent.
-          </p>
-        </Container>
-      </Jumbotron>
+        <Jumbotron className="hero">
+          <Container>
+            <h1 className="text-center text-white">Portfolio</h1>
+          </Container>
+        </Jumbotron>
     </Container>
   );
 };
