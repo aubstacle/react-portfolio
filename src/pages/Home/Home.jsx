03848d5ed040f170eadfa40e6040345a3a9323ef
Aubrey Polk <aubstacle3@gmail.com>
2021-01-15 01:03:09 -0500

    home jumbo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import Container from "../../components/Container/Container";
 import "./Home.css";
 import Row from "react-bootstrap/Row";
-import Jumbotron from "react-bootstrap/Jumbotron"
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 const Home = () => {
   return (
@@ -9,7 +9,10 @@ const Home = () => {
       className="flex-fill home-bg"
       containerType="container-fluid mb-5"
     >
-
+      <Jumbotron className="bg-dark text-center">
+        <p style={{color: 'white', fontSize:'30px'}}>Welcome!</p>
+        <h5 style={{color: 'white'}}>I'm a full-stack developer with previous experience in video, graphic design and all things digital media.</h5>
+      </Jumbotron>
     </Container>
   );
 };
